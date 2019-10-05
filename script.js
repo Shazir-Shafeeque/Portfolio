6b8040d790b448f714ceeb7333ec70061a35d49f
Shazir-Shafeeque <55185135+Shazir-Shafeeque@users.noreply.github.com>
2019-10-05 18:02:14 +0530

    "about-me" Worked on responsiveness for phones and tablets

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function onClickMenu(){
 	document.getElementById("blanket").classList.toggle("changeBlanket");
-	document.getElementById("menu").classList.toggle("change");
-	document.getElementById("nav").classList.toggle("change");
 	
 	document.getElementById("menu-bg").classList.toggle("change-bg");
+	
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
 }
+
+// function zoomIn(x){
+// 	x.style.width = "18vw";
+//     x.style.height = "calc((18vw / 43) * 57)";
+// }
